330da8348551ec87be985e9c73abdd6200c80dd2
Rafael Kasinski <rakasin@gmail.com>
2022-08-20 13:03:08 -0300

    Fixed API issue with Roberto's help

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
   validates :title, presence: true, uniqueness: { scope: :title }
   validates :overview, presence: true
 end
